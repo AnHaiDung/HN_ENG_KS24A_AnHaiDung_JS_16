b80f0a64d826dc698a586cb7397d166415e88cc9
AnHaiDung <anhaidung2k6@gmail.com>
2025-02-26 14:31:52 +0700

    Add files via upload

---
+++ b7/b7.js
@@ -0,0 +1,5 @@
+let math = +prompt("Nhap vao diem toan");
+let physics = +prompt("Nhap vao diem vat ly");
+let chemistry = +prompt("Nhap vao diem hoa");
+let avg = (math + physics + chemistry) / 3;
+document.write(`Trung binh cong 3 mon la: ${avg}`);
