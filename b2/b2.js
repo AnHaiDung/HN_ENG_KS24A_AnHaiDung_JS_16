710845dc32a09b6ac6cd93719e4c348eb5579f0e
AnHaiDung <anhaidung2k6@gmail.com>
2025-02-26 13:09:47 +0700

    Add files via upload

---
+++ b2/b2.js
@@ -0,0 +1,8 @@
+let number = 10;
+let price = 20.5;
+let status = true;
+let city = "Hà Nội";
+document.write(typeof number + "<br>");
+document.write(typeof price + "<br>");
+document.write(typeof status + "<br>");
+document.write(typeof city + "<br>");
